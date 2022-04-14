966232b2bbfd76f15d969283a2bba60aedf3f65f
YesterdayGrape <57185637+YesterdayGrape@users.noreply.github.com>
2022-04-14 19:10:41 +0000

    First draft of mesh refining process

--- main.py
+++ main.py
@@ -3,27 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import sqrt
 import timeit
+from scipy.spatial import Delaunay, ConvexHull
+#Delaunay no longer needed?
+import matplotlib.tri as mtri
 
 # 3d plotting on pyplot: https://jakevdp.github.io/PythonDataScienceHandbook/04.12-three-dimensional-plotting.html
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 
-"""
-# Data for a three-dimensional line
-zline = np.linspace(0, 15, 1000)
-xline = np.sin(zline)
-yline = np.cos(zline)
-ax.plot3D(xline, yline, zline, 'gray')
-
-# Data for three-dimensional scattered points
-zdata = 15 * np.random.random(100)
-xdata = np.sin(zdata) + 0.1 * np.random.randn(100)
-ydata = np.cos(zdata) + 0.1 * np.random.randn(100)
-ax.scatter3D(xdata, ydata, zdata, c=zdata, cmap='Greens');
-
-fig.savefig("model1.jpg")
-"""
 # Define wireframe boundary
 boundary = np.array([[0,1,1,1,1,0,0,0,0],
                      [0,0,0,1,1,1,1,0,0],
@@ -65,6 +53,15 @@ for i in range(1*n_side,4*n_side):
   linB = np.concatenate([np.linspace(boundary0[i+1,r:r+1], boundary0[9*n_side - i, r:r+1], n_side) for r in range(3)], axis=1)
   soap0 = np.append(soap0, linB, axis=0) #, axis=1)
 
+soap0 = np.unique(soap0, axis=0)
+
+
+
+#####################################################
+
+
+
+
 # Plot initial soap bubble
 # ax.scatter3D(soap0.T[0], soap0.T[1], soap0.T[2], c=[0 if i==0 else 1 for i in range(len(soap0))], cmap='Reds');
 print("Finished generating initial soap bubble")
@@ -82,7 +79,9 @@ REPS = 4
 # Display it as a surface or collection of points?
 surf = True
 # Number of points remains constant?
-constN = False
+constN = True#False
+# Used refining technique?
+sRefined = False#True
 
 def find_nearest(bubblepts0):
   dirs = []
@@ -114,21 +113,97 @@ def find_nearest(bubblepts0):
             vec_sum[coord] += (neighbor[coord] - pt[coord]) / mag * step
     
     dirs.append(vec_sum)
+
+  if constN:
+    return(dirs)
+  else:
+    return(dirs + len(boundary0)*[[0,0,0]])
+
+
+
+
+
+def area(tri):
+  return(0.5 * np.linalg.norm(np.cross(tri[0]-tri[1],tri[2]-tri[1])))
+
+def refine(bubblepts):
+  # Find the triangulation of the surface
+  #tris = Delaunay(bubblepts)
+  tris = ConvexHull(bubblepts)
+  #tris = mtri.Triangulation(bubblepts)
+  # Create an empty list in which to store the adjustments that need to be made to the mesh points
+  adjustments = [[] for j in range(len(bubblepts))]
+  # For each triangle, calculate the adjustments to its vertices based solely on that triangle
   
-  return(dirs + len(boundary0)*[[0,0,0]])
+  ax.plot_trisurf(bubblepts[:,0], bubblepts[:,1], bubblepts[:,2])#, tris.simplices)
+  fig.savefig("model3_2.jpg")
+  
+  for tri in tris.simplices:
+    #tris.triangles:#tris.simplices[:10]:
+    # Convert the indices stored in tris.simplices to actual points
+    triangle = [bubblepts[x] for x in tri]
+    # Calculate areas using cross product
+    # Triangle needs to be a list of numpy arrays
+    triArea = area(triangle)
+    
+    # Adjust in direction of that area for each vertex (sum two vectors and multiply by area swept out between them)
+    
+    # For each of the indices stored in tri (for each vertex)
+    for i,p in enumerate(tri):
+      # If the point is on the boundary and it's set to not increase the number of points:
+      print(triangle[i])
+      if any(np.equal(boundary0,triangle[i]).all(1)) and constN:
+        adjustments[p] = np.array([0], ndmin=1)
+        #print(p)
+        continue
 
+      #print("T", np.array(triArea * sum( \
+      #  [triangle[(g + i + 1) % 3] for g in range(2)]), ndmin=1))
+      # Add to the list of adjustments
+      adjustments[p].append(np.array(triArea * sum( \
+        [triangle[(g + i + 1) % 3] for g in range(2)]), ndmin=1))
+  #"""
+  print(adjustments[0:11])
+  #for k in adjustments:
+  #  print(k)
+  #print(np.array([len(k) for k in adjustments]))
+  #print([np.ndarray.size(n) for n in adjustments])
+  #"""
+  
+  #print([np.ndarray.size(n) for n in adjustments])
+  #print(len([np.sum(n) for n in adjustments]))
+  #print(([n for n in range(len(adjustments)) if len(adjustments[n])==0]))
+  #print(len([(step * (np.sum(n)) / len(n)) for n in adjustments]))
 
-def find_nearest0():
-  return(find_nearest(soap0))
+  #print(np.array([(step * (np.sum(n)) / len(n)) for n in adjustments]))
+  print(np.array([len(n) for n in adjustments]))
+  return(np.array([(step * (np.sum(n)) / len(n)) for n in adjustments]))
+  #return([step * (np.sum(n)) / np.ndarray.size(n) for n in adjustments])
+  #plt.triplot(bubblepts[:,0], bubblepts[:,1], bubblepts[:,2], tri.simplices)
 
 def bubble_evolve(bubblepts0, reps):
   soap = bubblepts0
   for rep in range(reps):
-    soap = np.add(np.concatenate([soap, boundary0], axis=0), find_nearest(soap))
+    #print(np.array(refine(soap))[:40])
+
+    
+    if sRefined == True:
+      if constN:
+        soap = np.add(soap, refine(soap))
+      else:
+        soap = np.add(np.concatenate([soap, boundary0], axis=0), refine(soap))
+
+    else:
+      if constN:
+        soap = np.add(soap, find_nearest(soap))
+      else:
+        soap = np.add(np.concatenate([soap, boundary0], axis=0), find_nearest(soap))
     print(f"Iteration: {rep + 1}")
   return soap
 
-"""
+
+
+#"""
 film = bubble_evolve(soap0, REPS)
 #print(find_nearest(soap0))
 
@@ -137,8 +212,17 @@ if surf:
 else:
   ax.scatter3D(film.T[0], film.T[1], film.T[2], c=[0 if i==0 else 1 for i in range(len(film))], cmap='Reds');
 # Save resulting figure as an image
-fig.savefig("model3_0.jpg")
-"""
+fig.savefig("model3_1.jpg")
+#"""
+
+
+
+
+
+
+def find_nearest0():
+  return(find_nearest(soap0))
+
 def find_nearest_time():
   SETUP_CODE = '''
 from __main__ import find_nearest0
@@ -167,6 +251,6 @@ bubble_evolve0()'''
   print (timeit.timeit(setup = SETUP_CODE,
                      stmt = TEST_CODE,
                      number = 1))
-bubble_evolve_time()
+# bubble_evolve_time()
 
-# bubble_evolve takes 34.5 sec, find_nearest * 4 takes 20 sec 
+# bubble_evolve with 4 iterations takes 34.5 sec, find_nearest * 4 takes 20 sec
